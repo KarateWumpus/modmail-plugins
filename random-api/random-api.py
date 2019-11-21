d95e90ef2baf0b2d0998ab67157a4e72adc2bf66
MiTonder <48181821+MiTonder@users.noreply.github.com>
2019-11-21 12:58:21 +0100

    New plugin (Still in testing phase)

---
+++ random-api/random-api.py
@@ -0,0 +1,50 @@
+import discord
+import asyncio
+import json
+from discord.ext import commands
+from core import checks
+from core.models import PermissionLevel
+
+
+class random-api(commands.Cog):
+    """This plugin uses https://some-random-api.ml to get a lotta random stuff"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    async def dog(self, ctx):
+        """Get a Dog fact and a Dog image"""
+        
+        getfact = await bot.session.get('https://some-random-api.ml/facts/dog')
+        getimg = await bot.session.get('https://some-random-api.ml/img/dog')
+        
+        facttext = await getfact.text()
+        imgtext = await getimg.text()
+        
+        factjson = json.loads(facttext)
+        imgjson = json.loads(imgtext)
+        
+        embed = discord.Embed(title = "Dog", description = factjson["fact"], color = 0x7289da)
+        embed.set_image(url=imgjson["link"])
+        await ctx.send(embed = embed)
+
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    async def cat(self, ctx):
+        """Get a Cat fact and a Cat image"""
+        
+        getfact = await bot.session.get('https://some-random-api.ml/facts/cat')
+        getimg = await bot.session.get('https://some-random-api.ml/img/cat')
+        
+        facttext = await getfact.text()
+        imgtext = await getimg.text()
+        
+        factjson = json.loads(facttext)
+        imgjson = json.loads(imgtext)
+        
+        embed = discord.Embed(title = "Cat", description = factjson["fact"], color = 0x7289da)
+        embed.set_image(url=imgjson["link"])
+        await ctx.send(embed = embed)
